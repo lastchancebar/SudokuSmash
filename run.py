f15163036d63b7da7c63294c38f98735592274d0
lastchancebar <darinaduffykelly@gmail.com>
2022-04-26 13:09:49 +0000

    add puzzle in lists

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+puzzle = [
+    [6, 0, 8,  0, 0, 3,  0, 2, 0],
+    [0, 4, 1,  0, 8, 0,  0, 0, 7],
+    [0, 0, 0,  0, 4, 5,  9, 6, 0],
+
+    [0, 1, 4,  0, 9, 0,  3, 0, 2],
+    [0, 0, 0,  2, 0, 4,  0, 0, 0],
+    [2, 0, 3,  0, 5, 0,  4, 7, 0],
+
+    [0, 2, 9,  3, 1, 0,  0, 8, 5],
+    [5, 0, 0,  0, 6, 0,  2, 0, 0],
+    [0, 3, 0,  5, 0, 0,  0, 0, 9]
+] 
+
