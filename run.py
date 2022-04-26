a19354d31312c1a1c56601f87a585b492b0715c9
lastchancebar <darinaduffykelly@gmail.com>
2022-04-26 21:33:42 +0000

    comple recursive algorithm to solve example board

--- run.py
+++ run.py
@@ -1,31 +1,43 @@
-puzzle = [
-    [6, 0, 8,  0, 0, 3,  0, 2, 0],
-    [0, 4, 1,  0, 8, 0,  0, 0, 7],
-    [0, 0, 0,  0, 4, 5,  9, 6, 0],
-
-    [0, 1, 4,  0, 9, 0,  3, 0, 2],
-    [0, 0, 0,  2, 0, 4,  0, 0, 0],
-    [2, 0, 3,  0, 5, 0,  4, 7, 0],
-
-    [0, 2, 9,  3, 1, 0,  0, 8, 5],
-    [5, 0, 0,  0, 6, 0,  2, 0, 0],
-    [0, 3, 0,  5, 0, 0,  0, 0, 9]
-] 
-
 def find_next_empty(puzzle):
     #find the next row, col on the puzzle that is empty - replace with 0
     # return row,col tuple (or(None, None) if there is none )
     # NB - 0-8 indices used here
     for r in range(9):
-        for c in rang(9):
-            if puzzle[r] [c] == 0:
+        for c in range(9):
+            if puzzle[r] [c]== 0:
                 return r, c
 
-    return None, None      # if no spaces left in the puzzle  
+    return None, None      # if no spaces left in the puzzle.  
 
 def is_valid(puzzle, guess, row, col):
     # if a valid guess returns True, otherwise False
 
+
+    # test the row
+    row_vals = puzzle[row]
+    if guess in row_vals:
+        return False
+
+    # test the columns
+    # col_vals = []
+    # for i in range(9):
+        # col_vals.append(puzzle[i] [col])
+    col_vals = [puzzle[i] [col] for i in range(9)]  
+    if guess in col_vals:
+        return False
+
+    # test each 3 x 3 matrix
+    # find where each square starts
+    row_start = (row // 3) * 3  #  1 // 3 = 0, 5 // 3 = 1,  8 // 3 = 2
+    col_start = (col // 3) * 3
+
+    for r in range(row_start, row_start + 3):
+        for c in range(col_start, col_start + 3):
+            if puzzle[r][c] == guess:
+                return False
+          
+    # if we get here, the checks pass, the guess may be valid so
+    return True    
 
 
 def solve_sudoku(puzzle):
@@ -40,8 +52,42 @@ def solve_sudoku(puzzle):
     if row is None: # if there are no spaces left
         return True
 
-   #step 2 : if there is a place to put a number, make a guess between 1-9
-   for guess in range(1,10): # range(1, 10) is 1- 9 inclusive 
-       # step 3: check if this is valid guess    
-       id is_valid(puzzle, guess, row, col):
+    # step 2 : if there is a place to put a number, make a guess between 1-9
+    for guess in range(1, 10): 
+        # range(1, 10) is 1- 9 inclusive
+        # step 3: check if this is valid guess    
+       if is_valid(puzzle, guess, row, col):
+            # step 3.1 : if this is valid, then place that guess in  the puzzle
+            puzzle[row][col] = guess
+            # now recurse using this puzzle
+            # step 4: recursively call our function
+            if solve_sudoku(puzzle):
+                return True
 
+        # step 5: if not valid  OR if the guess does not solve the puzzle, 
+        # then we need to backtrack and try a new number
+            puzzle[row][col] = 0   # reset guess
+
+    # step 6: if none of the numbers that we try work, then this 
+    # puzzle is unsolvable
+    return False
+
+
+if __name__ == '__main__':
+    example_board = [
+            [6, 0, 8,  0, 0, 3,  0, 2, 0],
+            [0, 4, 1,  0, 8, 0,  0, 0, 7],
+            [0, 0, 0,  0, 4, 5,  9, 6, 0],
+
+            [0, 1, 4,  0, 9, 0,  3, 0, 2],
+            [0, 0, 0,  2, 0, 4,  0, 0, 0],
+            [2, 0, 3,  0, 5, 0,  4, 7, 0],
+
+            [0, 2, 9,  3, 1, 0,  0, 8, 5],
+            [5, 0, 0,  0, 6, 0,  2, 0, 0],
+            [0, 3, 0,  5, 0, 0,  0, 0, 9]
+        ] 
+    print(solve_sudoku(example_board))
+    print(example_board)
+
+   
